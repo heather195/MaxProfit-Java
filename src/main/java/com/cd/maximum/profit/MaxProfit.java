af1df7ad7966549df29bb672498a3f712ce595e2
Heather <heather.craddock@hotmail.co.uk>
2020-10-05 15:16:11 -0400

    solution

--- src/main/java/com/cd/maximum/profit/MaxProfit.java
+++ src/main/java/com/cd/maximum/profit/MaxProfit.java
@@ -3,6 +3,22 @@ package com.cd.maximum.profit;
 public class MaxProfit {
 
     public Integer find(Integer[] input){
-        return null;
+        int buyPrice, sellPrice, tempProfit;
+        int maxProfit = -1;
+        int buyIndex = 0;
+
+        for(int i = 0; i < input.length-1; i++){
+            buyPrice = input[buyIndex];
+            sellPrice = input[i+1];
+            if(buyPrice > sellPrice){
+                buyIndex = i+1;
+            } else {
+                tempProfit = sellPrice - buyPrice;
+                if(tempProfit > maxProfit){
+                    maxProfit = tempProfit;
+                }
+            }
+        }
+        return maxProfit;
     }
 }
